ac6af8753c7dd43c6d4a223318ff1ee82722d31b
arano1212 <angelgonzalezarano@MacBook-Air-de-Angel.local>
2023-08-30 22:42:28 -0600

    ejercicio2

---
+++ app.js
@@ -0,0 +1,35 @@
+//arrays
+//numero 1
+/*let numeroAleatorios =[];
+for (let i = 0; i <10; i++) {
+    let numeroAleatorio = Math.random();
+    numeroAleatorios.push(numeroAleatorio);
+
+console.log(numeroAleatorios);
+}
+
+//numero2
+let usuario = prompt(`ingresa  1,2,3,4,5,`);
+let miArray = usuario.split(',');
+console.log(miArray);*/
+
+//numero 3
+/*let numDesordenado= [10,40,30,20,15,5];
+numDesordenado.sort(function(a,b) {
+    return a-b; 
+});
+console.log(numDesordenado);
+
+let numMax = Math.max(...numDesordenado);
+let numMin =Math.min(...numDesordenado);
+
+console.log(`El numero mayor ${numMax}`);
+console.log(`El numero menor ${numMin}`);*/
+
+
+
+
+
+
+
+
